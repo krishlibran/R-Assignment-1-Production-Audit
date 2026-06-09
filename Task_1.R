81239be64581f4b6c48fe93b65b47dcfa56366d9
krishlibran <krishlibran@gmail.com>
2026-06-09 22:22:16 +0530

    Create Task_1.R

---
+++ Task_1.R
@@ -0,0 +1,5 @@
+library(dplyr)
+production <- read.csv("jv_production_logs.csv")
+summary(production)
+Downtime_operator <- sum(is.na(production$Downtime_Minutes))
+cat(Downtime_operator, "operators forgot to log in their downtime")

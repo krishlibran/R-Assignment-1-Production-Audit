72113a9397d3edde53cb9a8b8c6beb41c59f43d2
krishlibran <krishlibran@gmail.com>
2026-06-10 22:40:17 +0530

    Update Task_1.R

--- Task_1.R
+++ Task_1.R
@@ -11,3 +11,10 @@ cat(Downtime_operator, "operators forgot to log in their downtime") #3
 major_stops <- filter(production, Downtime_Minutes > 30)
 # Verify if the major stops are correct
 all(major_stops$Downtime_Minutes > 30) # Returns TRUE
+
+# Task 3: Prioritize by Defect. Take the filtered data from Task 2 
+# and use arrange() to sort it from highest to lowest by Defective_Parts. 
+# This tells us which broken machine is bleeding the most money.
+major_stops$Machine_Type = gsub("cnc_lathe", "CNC_Lathe", major_stops$Machine_Type) # Correction of typing error that is replacing "cnc_lathe to CNC_Lathe in the data frame.
+head(arrange(major_stops[, c("Machine_Type", "Defective_Parts")], desc(Defective_Parts)), 5) # Selecting the top 5 rows after arranging the Defective_Parts column in Descending order
+arrange(summarise(group_by(major_stops, Machine_Type), Total_Defective_Parts = sum(Defective_Parts), Number_of_Operators = n()), desc(Total_Defective_Parts)) # Categorising the Machine_Type column and getting the Total_Defective_Parts of each Machine_Type
